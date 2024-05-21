34975664d141e34801342b8519bcd75e2d3f2eff
KostiantynAnanchenko <itemswap@gmail.com>
2024-05-21 14:03:22 +0200

    1

--- src/test/java/Tasks/CalculatorTest.java
+++ src/test/java/Tasks/CalculatorTest.java
@@ -1,4 +1,30 @@
+package Tasks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-class CalculatorTest {
-  
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testSumForOne() {
+        assertEquals(1, calculator.sum(1));
+    }
+
+    @Test
+    public void testSumForThree() {
+        assertEquals(6, calculator.sum(3));
+    }
+
+    @Test
+    public void testSumForZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.sum(0));
+    }
 }
