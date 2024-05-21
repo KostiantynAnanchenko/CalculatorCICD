431536d8217332774092a71f98ae43b2566ef9b9
KostiantynAnanchenko <itemswap@gmail.com>
2024-05-21 14:27:49 +0200

    Some changes

--- src/main/java/Tasks/Calculator.java
+++ src/main/java/Tasks/Calculator.java
@@ -2,15 +2,15 @@ package Tasks;
 
 public class Calculator {
 
+
     public int sum (int n){
         if (n <= 0) {
             throw new IllegalArgumentException("n should be a higher than zero");
         }
         int result =0;
         for(int i=0;i<=n;i++) {
-            result-=i;
+            result+=i;
         }
-        System.out.println("New feature");
         return result;
     }
 
